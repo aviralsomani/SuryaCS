6345e73de4e18ea1deede97812d4d0330939aa26
Aviral Somani <avisomani@Gyanada.local>
2018-12-30 17:06:30 -0600

    adding search files

---
+++ Searches/src/SearchableContainer.java
@@ -0,0 +1,41 @@
+
+public class SearchableContainer
+{
+	private int[] arr;
+	private int size;
+	private int capacity;
+	
+	public SearchableContainer() {
+		
+	}
+	
+	public void add(int value) {
+		
+	}
+	
+	public void remove(int value) {
+		
+	}
+	
+	public int size() {
+		return 0;
+	}
+	
+	public int sequentialSearch(int value) {
+		return -1;
+	}
+	
+	public int binarySearch(int value) {
+		return -1;
+	}
+	
+	@Override
+	public String toString() {
+		return "";
+	}
+
+	public static void main(String[] args) {
+		String s = "Hello, World!";
+		System.out.println(s.substring(2, 9));
+	}
+}
